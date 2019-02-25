f967a12117b9a62bb28cacbcc455747e5bb6c57c
JackLovel <2101721230@qq.com>
2019-02-25 13:16:23 +0800

    3.4

---
+++ BashShell/ch3/3.4/remove_duplicates.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#文件名： remove_duplicates.sh
+# 作途：查找并删除重复文件,第一个文件只保留一份
+
+ls -1S --time-style=long-iso | awk 'BEGIN' {
+        getline; getline;
+        name1=$8; size=$5
+    }
+
+{
+    name2=$8;
+    if (size==$5)
+    {
+        "md5sum "name1 | getline; csum1=$1;
+        "md5sum "name2 | getline; csum2=$1;
+        if (csum1==csum2)
+        {
+            print name1; print name2
+        }
+    };
+
+    size=$5; name1=name2;
+}' | sort -u > duplicate_files
+
+cat duplicate_files | xargs -I {} md5sum {} | sort | uniq -w 32 | awk '{ print "^"$2"$" }' | sort -u > duplicate_sample
+
+echo Removing...
+echo duplicate_file duplicate_sample -2 -3 | tee /dev/stderr | xargs rm
+echo Removed duplicates file successfully.
