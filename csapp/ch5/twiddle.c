42b9a27888caaaca6fdb65077e4ae9b08c289e4a
JackLovel <2101721230@qq.com>
2019-05-03 23:27:44 +0800

    update

---
+++ csapp/ch5/twiddle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void twiddle1(long *xp, long *yp)
+{
+    *xp += *yp;
+    *xp += *yp;
+}
+
+void twiddle2(long *xp, long *yp)
+{
+    *xp += 2* *yp;
+}
+
+int main()
+{
+    return 0;
+}
